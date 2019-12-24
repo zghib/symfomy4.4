7a41ca2545c6760326d3d24bd10813d4452afb2b
Ion Zghibarta <ion.zghibarta@ellation.com>
2019-12-24 10:27:08 +0200

    Add Security

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController
+{
+    /**
+     * @Route("/index")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function index()
+    {
+        return new JsonResponse(['message' => 'Success']);
+    }
+}
